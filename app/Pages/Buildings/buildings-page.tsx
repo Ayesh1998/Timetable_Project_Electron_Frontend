9d4d2351fc6a8bc904bd4a61895237ddc2f82b51
TharindaNimnajith <tharindarajapakshe@gmail.com>
2020-08-18 23:39:09 +0530

    resolve conflicts

--- app/Pages/Buildings/buildings-page.tsx
+++ app/Pages/Buildings/buildings-page.tsx
@@ -1,23 +1,11 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-// import BuildingsAdd from './buildings-add'
-// import BuildingsEdit from './buildings-edit'
-import BuildingsList from './buildings-list';
+import React from 'react'
 
 const BuildingsPage: React.FC = () => {
-  const editBuilding = useSelector(
-    (state: { buildings: any; editBuilding: boolean }) =>
-      state.buildings.editBuilding
-  );
-
   return (
-    <div className="container">
+    <div className='container'>
       <h1>Buildings</h1>
-      {/* <BuildingsAdd/> */}
-      {/* <BuildingsEdit/> */}
-      <BuildingsList />
     </div>
   );
 };
 
-export default BuildingsPage;
+export default BuildingsPage
--- app/Pages/Rooms/rooms-page.tsx
+++ app/Pages/Rooms/rooms-page.tsx
@@ -1,22 +1,11 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-// import RoomsAdd from './rooms-add'
-// import RoomsEdit from './rooms-edit'
-import RoomsList from './rooms-list';
+import React from 'react'
 
 const RoomsPage: React.FC = () => {
-  const editRoom = useSelector(
-    (state: { rooms: any; editRoom: boolean }) => state.rooms.editRoom
-  );
-
   return (
-    <div className="container">
+    <div className='container'>
       <h1>Rooms</h1>
-      {/* <RoomsAdd/> */}
-      {/* <RoomsEdit/> */}
-      <RoomsList />
     </div>
-  );
-};
+  )
+}
 
-export default RoomsPage;
+export default RoomsPage
