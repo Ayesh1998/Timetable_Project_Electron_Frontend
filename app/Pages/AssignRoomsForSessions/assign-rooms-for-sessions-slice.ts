554a0f715a7c4224c3a73442df066980c52bbbc7
TharindaNimnajith <tharindarajapakshe@gmail.com>
2020-09-19 19:06:34 +0530

    rooms assign to sessions basic

--- app/Pages/AssignRoomsForSessions/assign-rooms-for-sessions-slice.ts
+++ app/Pages/AssignRoomsForSessions/assign-rooms-for-sessions-slice.ts
@@ -2,8 +2,16 @@ import {createSlice} from '@reduxjs/toolkit'
 
 const setAssignRoomsForSessionsSlice = createSlice({
   name: 'assignRoomsForSessions',
-  initialState: {},
-  reducers: {}
+  initialState: {
+    sessions: {}
+  },
+  reducers: {
+    setSessions: (state, action: any) => {
+      state.sessions = action.payload
+    }
+  }
 })
+
+export const {setSessions} = setAssignRoomsForSessionsSlice.actions
 
 export default setAssignRoomsForSessionsSlice.reducer
